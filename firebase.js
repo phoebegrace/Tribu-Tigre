58b5db4d2002d16d5a86c7886cacbf10cdc4a7f4
Phoebe Grace Juayong <pebi@Phoebes-MacBook-Air.local>
2025-10-01 23:28:30 +0800

    Fix Firebase storageBucket domain for SDK

--- firebase.js
+++ firebase.js
@@ -4,7 +4,7 @@ window.firebaseConfig = {
   apiKey: "AIzaSyBpIXhE7vebD-GqTxsK-cCWg84VM1SZDmo",
   authDomain: "tributigre-d7006.firebaseapp.com",
   projectId: "tributigre-d7006",
-  storageBucket: "tributigre-d7006.firebasestorage.app",
+  storageBucket: "tributigre-d7006.appspot.com",
   messagingSenderId: "341539964799",
   appId: "1:341539964799:web:0475085787c7e6f9dcbc4a",
   measurementId: "G-H4S73Z5H6L"
